d08df80920e4917721ce2f103f6e64957eb8b835
kazhamiakin <kazhamiakin@gmail.com>
2019-11-19 16:57:24 +0100

    Connector refactoring

---
+++ connectors/orgmanager-apimconnector/src/main/java/it/smartcommunitylab/apimconnector/utils/Utils.java
@@ -0,0 +1,113 @@
+package it.smartcommunitylab.apimconnector.utils;
+
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.apache.axis2.client.ServiceClient;
+import org.apache.axis2.java.security.SSLProtocolSocketFactory;
+import org.apache.axis2.transport.http.HTTPConstants;
+import org.apache.commons.httpclient.protocol.Protocol;
+import org.apache.commons.httpclient.protocol.ProtocolSocketFactory;
+import org.apache.http.ssl.TrustStrategy;
+import org.wso2.carbon.user.mgt.common.xsd.ClaimValue;
+
+public class Utils {
+
+	private static final String PROD_SP = "%s_%s_PRODUCTION";
+	private static final String SAND_SP = "%s_%s_SANDBOX";
+	
+	private static final String SUPER_TENANT = "carbon.super";
+	
+//	public static String getUserLocalName() {
+//		String principal = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//		return getLocalName(principal);
+//	}
+//	public static String getUserFullName() {
+//		String principal = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//		return principal;
+//	}
+	public static String getUserTenantName(String userNameWithTenant) {
+		return getTenantName(userNameWithTenant);
+	}
+	public static String getUserNormalizedName(String user) {
+		if (user.endsWith("@"+SUPER_TENANT)) return user.substring(0,  user.lastIndexOf('@'));
+		return user;
+	}
+	
+	public static String getUserNameAtSuperTenant(String username) {
+		return username + "@" + SUPER_TENANT;
+	}
+	
+	public static String getUserNameAtTenant(String username, String tenantName) {
+		return username + "@" + tenantName;
+	}
+	
+	
+	/**
+	 * @param principal
+	 * @return
+	 */
+	private static String getTenantName(String principal) {
+		if (principal.indexOf('@') > 0) return principal.substring(principal.lastIndexOf('@')+1);
+		return SUPER_TENANT;
+	}
+
+	/**
+	 * @param appName
+	 * @return
+	 */
+	public static String getProductionSP(String appName, String username) {
+		return String.format(PROD_SP, username, appName);
+	}
+	
+	/**
+	 * @param appName
+	 * @return
+	 */
+	public static String getSandboxSP(String appName, String username) {
+		return String.format(SAND_SP, username, appName);
+	}
+
+	public static ClaimValue createClaimValue(String uri, String value) {
+		ClaimValue cv = new ClaimValue();
+		cv.setClaimURI(uri);
+		cv.setValue(value);
+		return cv;
+	}
+	
+	public static void disableSSLValidator(ServiceClient client) {
+		
+		// Create a trust manager that does not validate certificate chains
+	    final TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+
+	            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+	                return null;
+	            }
+
+	            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+	            }
+
+	            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+	            }
+	        }
+	    };
+		TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+
+	    // Install the all-trusting trust manager
+	    try {
+			SSLContext sc = org.apache.http.ssl.SSLContexts.custom()
+			        .loadTrustMaterial(null, acceptingTrustStrategy)
+			        .build();
+
+	        sc.init(null, trustAllCerts, null);
+	        SSLProtocolSocketFactory sslFactory = new SSLProtocolSocketFactory(sc);
+	        Protocol prot = new Protocol("https", (ProtocolSocketFactory) sslFactory, 443);
+	        client.getOptions().setProperty(HTTPConstants.CUSTOM_PROTOCOL_HANDLER, prot);
+	    } catch (Exception ex) {
+	        // take action
+	    }
+	}
+}
