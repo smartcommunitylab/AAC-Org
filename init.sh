ddb4763c000fafb1561ad617df3e814bd564daa0
Matteo Saloni <matteo.saloni@gmail.com>
2019-12-12 15:00:48 +0100

    prepare for build

--- init.sh
+++ init.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-if [ -z "${APIM_HOSTNAME}" ]; then
-  echo "APIM_HOSTNAME is not set"
-else
-  rm -f cert.pem && echo -n | openssl s_client -connect ${APIM_HOSTNAME}:9443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ./cert.pem
-  keytool -import -trustcacerts -file cert.pem -alias root -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass changeit -noprompt
-fi
-java -jar app.jar
+exec java -XX:MaxRAMFraction=2 -jar app.jar
