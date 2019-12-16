2adbed32604fae23de90c5f6b124b64d0ffac963
Matteo Saloni <matteo.saloni@gmail.com>
2019-12-16 17:27:46 +0100

    add resolver for single page resources

--- server/src/main/java/it/smartcommunitylab/orgmanager/config/WebConfig.java
+++ server/src/main/java/it/smartcommunitylab/orgmanager/config/WebConfig.java
@@ -1,13 +1,57 @@
 package it.smartcommunitylab.orgmanager.config;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("GET", "PUT", "POST", "DELETE");
-	}
+
+    private final static String apiPath = "/api";
+
+    @Autowired
+    private ResourceProperties resourceProperties;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .allowedHeaders("Content-Type", "Authorization");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(resourceProperties.getStaticLocations())
+                .resourceChain(true)
+                .addResolver(new SinglePageAppResourceResolver());
+    }
+
+    private class SinglePageAppResourceResolver extends PathResourceResolver {
+        @Override
+        protected Resource getResource(String resourcePath, Resource location) throws IOException {
+            Resource resource = location.createRelative(resourcePath);
+
+            if (resource.exists() && resource.isReadable()) {
+                return resource;
+            } else if (("/" + resourcePath).startsWith(apiPath)) {
+                return null;
+            } else {
+                resource = location.createRelative("index.html");
+                if (resource.exists() && resource.isReadable()) {
+                    return resource;
+                } else {
+                    return null;
+                }
+            }
+        }
+    }
 }
