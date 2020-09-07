9c0f8305f3ad58e607fc8846a711d779770af783
Matteo Saloni <matteo.saloni@gmail.com>
2020-09-07 17:31:47 +0200

    cleanup attic, remove from branch old accessory code

--- attic/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ -z "${APIM_HOSTNAME}" ]; then
-  echo "APIM_HOSTNAME is not set"
-else
-  rm -f cert.pem && echo -n | openssl s_client -connect ${APIM_HOSTNAME}:9443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ./cert.pem
-  keytool -import -trustcacerts -file cert.pem -alias root -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass changeit -noprompt
-fi
-java -jar app.jar
