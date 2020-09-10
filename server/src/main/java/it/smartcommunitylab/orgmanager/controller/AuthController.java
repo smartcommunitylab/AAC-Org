e5285c83fcc7f7ca6f729a4e138147623c792bfc
Matteo Saloni <matteo.saloni@gmail.com>
2020-09-10 16:25:11 +0200

    comment legacy code

--- server/src/main/java/it/smartcommunitylab/orgmanager/controller/AuthController.java
+++ server/src/main/java/it/smartcommunitylab/orgmanager/controller/AuthController.java
@@ -53,201 +53,201 @@ import it.smartcommunitylab.orgmanager.service.ProfileService;
 public class AuthController {
 
     private final static Logger logger = LoggerFactory.getLogger(AuthController.class);
-
-    @Value("${security.oauth2.client.user-authorization-uri}")
-    private String authorizationURL;
-
-    @Value("${security.oauth2.client.access-token-uri}")
-    private String tokenURL;
-
-    @Value("${security.oauth2.client.client-id}")
-    private String clientId;
-
-    @Value("${security.oauth2.client.client-secret}")
-    private String clientSecret;
-
-    @Value("${security.oauth2.client.scopes}")
-    private String oauthScopes;
-
-    @Value("${application.url}")
-    private String applicationURL;
-
-    @Value("${client.url}")
-    private String clientURL;
-
-    @Value("${aac.uri}")
-    private String aacUri;
-
-    @Autowired
-    private ProfileService profileService;
-
-    @GetMapping(value = "/api/auth/user", produces = "application/json")
-    public UserRightsDTO getUserRights() {
-        logger.trace("called userrights");
-        UserRightsDTO dto = OrgManagerUtils.getUserRights();
-        logger.trace(dto.toString());
-        return dto;
-    }
-
-    /*
-     * Login
-     */
-
-    @GetMapping(value = "/api/auth/login", produces = "application/json")
-    @ApiOperation(value = "Login users via OAuth")
-    public RedirectView login(RedirectAttributes attributes,
-            HttpServletRequest request, HttpServletResponse response) {
-
-        String currentURL = request.getRequestURL().toString();
-        String callbackURL = currentURL.replace("/auth/login", "/auth/callback");
-
-        if (!applicationURL.isEmpty()) {
-            callbackURL = applicationURL.concat("/api/auth/callback");
-        }
-
-        // build authorization parameters
-        attributes.addAttribute("response_type", "code");
-        attributes.addAttribute("client_id", clientId);
-        attributes.addAttribute("scope", oauthScopes);
-        attributes.addAttribute("redirect_uri", callbackURL);
-        // TODO create and store state var
-
-        logger.debug("send redirect to oauth at " + authorizationURL);
-        logger.debug(attributes.asMap().toString());
-
-        return new RedirectView(authorizationURL, false);
-    }
-
-    @GetMapping(value = "/api/auth/callback", produces = "application/json")
-    @ApiOperation(value = "Login callback via OAuth")
-    public void callback(RedirectAttributes attributes,
-            HttpServletRequest request, HttpServletResponse response) throws LoginException {
-
-        String currentURL = request.getRequestURL().toString();
-        String redirectURI = currentURL;
-//        String redirectURL = "/#/callback";
-        String redirectURL = currentURL.replace("/api/auth/callback", "/");
-
-        if (!applicationURL.isEmpty()) {
-            redirectURI = applicationURL.concat("/api/auth/callback");
-            redirectURL = applicationURL.concat("/");
-        }
-
-        if (!clientURL.isEmpty()) {
-            redirectURL = clientURL.concat("/");
-        }
-
-        String code = request.getParameter("code");
-        String state = request.getParameter("state");
-
-        if (code == null || code.isEmpty()) {
-            throw new LoginException("invalid code");
-        }
-
-        logger.debug("oauth callback");
-        logger.trace("oauth authorization code " + code);
-
-        RestTemplate template = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        // buid auth header as basic
-        String auth = clientId + ":" + clientSecret;
-        byte[] encodedAuth = Base64.encodeBase64(
-                auth.getBytes(Charset.forName("UTF-8")));
-        String authHeader = "Basic " + new String(encodedAuth);
-        headers.set("Authorization", authHeader);
-
-        // build token parameters
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
-        map.add("grant_type", "authorization_code");
-        map.add("code", code);
-//        map.add("client_id", clientId);
-//        map.add("client_secret", clientSecret);
-        map.add("redirect_uri", redirectURI);
-
-        logger.trace("call token url at " + tokenURL);
-        logger.trace(map.toString());
-
-        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<MultiValueMap<String, String>>(map, headers);
-
-        ResponseEntity<String> result = template.exchange(tokenURL, HttpMethod.POST, entity, String.class);
-        if (result.getStatusCode() != HttpStatus.OK) {
-            logger.error("token response code " + String.valueOf(result.getStatusCodeValue()));
-            logger.debug(result.getBody());
-            throw new LoginException("invalid token response");
-        }
-
-        try {
-            // parse as json
-            JSONObject json = new JSONObject(result.getBody());
-
-            logger.trace(json.toString());
-
-            // extract tokens
-            String accessToken = json.optString("access_token", "");
-            String refreshToken = json.optString("refresh_token", "");
-            String expiresIn = json.optString("expires_in", "");
-
-            logger.trace("access token " + accessToken);
-            logger.trace("refresh token " + refreshToken);
-
-            if (accessToken.isEmpty()) {
-                logger.error("empty access token");
-                throw new LoginException("empty access token");
-            }
-
-            // fetch userInfo
-            // TODO if needed
-
-            // append token - should be already urlencoded
-            redirectURL = redirectURL.concat("#access_token=" + accessToken + "&expires_in=" + expiresIn);
-
-            logger.debug("send redirect to " + redirectURL);
-            response.sendRedirect(redirectURL);
-
-        } catch (JSONException jex) {
-            logger.error("json parsing error " + jex.getMessage());
-            throw new LoginException("response error");
-        } catch (IOException iex) {
-            logger.error("io error " + iex.getMessage());
-            throw new LoginException("network error");
-        }
-    }
-
-    @GetMapping(value = "/api/auth/profile", produces = "application/json")
-    @ApiOperation(value = "Get user profile")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "Authorization", value = "Access Token", required = true, allowEmptyValue = false, paramType = "header", dataTypeClass = String.class, example = "Bearer access_token")
-    })
-    @ResponseBody
-    public BasicProfile userProfile(
-            HttpServletRequest request, HttpServletResponse response)
-            throws LoginException, IdentityProviderAPIException, NoSuchUserException {
-
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth == null) {
-            throw new LoginException();
-        }
-
-        String userName = OrgManagerUtils.getAuthenticatedUserId();
-        logger.info("user name " + userName);
-        BasicProfile profile = profileService.getUserProfileById(userName);
-        logger.debug(profile.toString());
-        return profile;
-    }
-
-    @ExceptionHandler(LoginException.class)
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    @ResponseBody
-    public String loginError(LoginException ex) {
-        return ex.getMessage();
-    }
-
-    @ExceptionHandler(NoSuchUserException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ResponseBody
-    public String notFound(NoSuchUserException ex) {
-        return ex.getMessage();
-    }
+//
+//    @Value("${security.oauth2.client.user-authorization-uri}")
+//    private String authorizationURL;
+//
+//    @Value("${security.oauth2.client.access-token-uri}")
+//    private String tokenURL;
+//
+//    @Value("${security.oauth2.client.client-id}")
+//    private String clientId;
+//
+//    @Value("${security.oauth2.client.client-secret}")
+//    private String clientSecret;
+//
+//    @Value("${security.oauth2.client.scopes}")
+//    private String oauthScopes;
+//
+//    @Value("${application.url}")
+//    private String applicationURL;
+//
+//    @Value("${client.url}")
+//    private String clientURL;
+//
+//    @Value("${aac.uri}")
+//    private String aacUri;
+//
+//    @Autowired
+//    private ProfileService profileService;
+//
+//    @GetMapping(value = "/api/auth/user", produces = "application/json")
+//    public UserRightsDTO getUserRights() {
+//        logger.trace("called userrights");
+//        UserRightsDTO dto = OrgManagerUtils.getUserRights();
+//        logger.trace(dto.toString());
+//        return dto;
+//    }
+//
+//    /*
+//     * Login
+//     */
+//
+//    @GetMapping(value = "/api/auth/login", produces = "application/json")
+//    @ApiOperation(value = "Login users via OAuth")
+//    public RedirectView login(RedirectAttributes attributes,
+//            HttpServletRequest request, HttpServletResponse response) {
+//
+//        String currentURL = request.getRequestURL().toString();
+//        String callbackURL = currentURL.replace("/auth/login", "/auth/callback");
+//
+//        if (!applicationURL.isEmpty()) {
+//            callbackURL = applicationURL.concat("/api/auth/callback");
+//        }
+//
+//        // build authorization parameters
+//        attributes.addAttribute("response_type", "code");
+//        attributes.addAttribute("client_id", clientId);
+//        attributes.addAttribute("scope", oauthScopes);
+//        attributes.addAttribute("redirect_uri", callbackURL);
+//        // TODO create and store state var
+//
+//        logger.debug("send redirect to oauth at " + authorizationURL);
+//        logger.debug(attributes.asMap().toString());
+//
+//        return new RedirectView(authorizationURL, false);
+//    }
+//
+//    @GetMapping(value = "/api/auth/callback", produces = "application/json")
+//    @ApiOperation(value = "Login callback via OAuth")
+//    public void callback(RedirectAttributes attributes,
+//            HttpServletRequest request, HttpServletResponse response) throws LoginException {
+//
+//        String currentURL = request.getRequestURL().toString();
+//        String redirectURI = currentURL;
+////        String redirectURL = "/#/callback";
+//        String redirectURL = currentURL.replace("/api/auth/callback", "/");
+//
+//        if (!applicationURL.isEmpty()) {
+//            redirectURI = applicationURL.concat("/api/auth/callback");
+//            redirectURL = applicationURL.concat("/");
+//        }
+//
+//        if (!clientURL.isEmpty()) {
+//            redirectURL = clientURL.concat("/");
+//        }
+//
+//        String code = request.getParameter("code");
+//        String state = request.getParameter("state");
+//
+//        if (code == null || code.isEmpty()) {
+//            throw new LoginException("invalid code");
+//        }
+//
+//        logger.debug("oauth callback");
+//        logger.trace("oauth authorization code " + code);
+//
+//        RestTemplate template = new RestTemplate();
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+//
+//        // buid auth header as basic
+//        String auth = clientId + ":" + clientSecret;
+//        byte[] encodedAuth = Base64.encodeBase64(
+//                auth.getBytes(Charset.forName("UTF-8")));
+//        String authHeader = "Basic " + new String(encodedAuth);
+//        headers.set("Authorization", authHeader);
+//
+//        // build token parameters
+//        MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+//        map.add("grant_type", "authorization_code");
+//        map.add("code", code);
+////        map.add("client_id", clientId);
+////        map.add("client_secret", clientSecret);
+//        map.add("redirect_uri", redirectURI);
+//
+//        logger.trace("call token url at " + tokenURL);
+//        logger.trace(map.toString());
+//
+//        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+//
+//        ResponseEntity<String> result = template.exchange(tokenURL, HttpMethod.POST, entity, String.class);
+//        if (result.getStatusCode() != HttpStatus.OK) {
+//            logger.error("token response code " + String.valueOf(result.getStatusCodeValue()));
+//            logger.debug(result.getBody());
+//            throw new LoginException("invalid token response");
+//        }
+//
+//        try {
+//            // parse as json
+//            JSONObject json = new JSONObject(result.getBody());
+//
+//            logger.trace(json.toString());
+//
+//            // extract tokens
+//            String accessToken = json.optString("access_token", "");
+//            String refreshToken = json.optString("refresh_token", "");
+//            String expiresIn = json.optString("expires_in", "");
+//
+//            logger.trace("access token " + accessToken);
+//            logger.trace("refresh token " + refreshToken);
+//
+//            if (accessToken.isEmpty()) {
+//                logger.error("empty access token");
+//                throw new LoginException("empty access token");
+//            }
+//
+//            // fetch userInfo
+//            // TODO if needed
+//
+//            // append token - should be already urlencoded
+//            redirectURL = redirectURL.concat("#access_token=" + accessToken + "&expires_in=" + expiresIn);
+//
+//            logger.debug("send redirect to " + redirectURL);
+//            response.sendRedirect(redirectURL);
+//
+//        } catch (JSONException jex) {
+//            logger.error("json parsing error " + jex.getMessage());
+//            throw new LoginException("response error");
+//        } catch (IOException iex) {
+//            logger.error("io error " + iex.getMessage());
+//            throw new LoginException("network error");
+//        }
+//    }
+//
+//    @GetMapping(value = "/api/auth/profile", produces = "application/json")
+//    @ApiOperation(value = "Get user profile")
+//    @ApiImplicitParams({
+//            @ApiImplicitParam(name = "Authorization", value = "Access Token", required = true, allowEmptyValue = false, paramType = "header", dataTypeClass = String.class, example = "Bearer access_token")
+//    })
+//    @ResponseBody
+//    public BasicProfile userProfile(
+//            HttpServletRequest request, HttpServletResponse response)
+//            throws LoginException, IdentityProviderAPIException, NoSuchUserException {
+//
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        if (auth == null) {
+//            throw new LoginException();
+//        }
+//
+//        String userName = OrgManagerUtils.getAuthenticatedUserId();
+//        logger.info("user name " + userName);
+//        BasicProfile profile = profileService.getUserProfileById(userName);
+//        logger.debug(profile.toString());
+//        return profile;
+//    }
+//
+//    @ExceptionHandler(LoginException.class)
+//    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+//    @ResponseBody
+//    public String loginError(LoginException ex) {
+//        return ex.getMessage();
+//    }
+//
+//    @ExceptionHandler(NoSuchUserException.class)
+//    @ResponseStatus(HttpStatus.NOT_FOUND)
+//    @ResponseBody
+//    public String notFound(NoSuchUserException ex) {
+//        return ex.getMessage();
+//    }
 }
